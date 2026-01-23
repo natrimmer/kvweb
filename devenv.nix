967e9a1f1508d45bfcfb1df6c76d7c8e85da598a
natrimmer <nicholas.trimmer@mcgovern.org>
2026-01-22 16:09:39 -0800

    feat: configure development environment for kvweb project

--- devenv.nix
+++ devenv.nix
@@ -1,22 +1,26 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, ... }:
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env.GREET = "kvweb";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.gopls
+    pkgs.golangci-lint
+    pkgs.nodejs
+    pkgs.pnpm
+    pkgs.nixd
+    pkgs.nil
+  ];
 
   # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+  languages = {
+    go.enable = true;
+    typescript.enable = true;
+  };
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
   scripts.hello.exec = ''
     echo hello from $GREET
   '';
@@ -24,23 +28,10 @@
   # https://devenv.sh/basics/
   enterShell = ''
     hello         # Run scripts directly
-    git --version # Use packages
   '';
 
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
